b27efcbbec1bd00c12a44a6106338ea6f96cb57d
java_khan@126.com <khan7923>
2017-08-14 17:55:32 +0800

    init

---
+++ clodu-zuul/src/test/java/com/imkzp/clodu/zuul/CloduZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imkzp.clodu.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloduZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
