79ba83e429eeec513fa2796fe875a04e257e9c7c
java_khan@126.com <khan7923>
2017-08-14 18:02:30 +0800

    init

---
+++ cloud-service-report/src/main/java/com/imkzp/cloud/service/report/CloudServiceReportApplication.java
@@ -0,0 +1,12 @@
+package com.imkzp.cloud.service.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServiceReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServiceReportApplication.class, args);
+	}
+}
